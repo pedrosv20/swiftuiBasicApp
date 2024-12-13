c10fa2dea129e70cca43f049fa27523f81f9c1c4
Pedro <pedro1silva1vargas@gmail.com>
2024-12-13 17:59:04 -0300

    Create repo

---
+++ PersonalProject/PersonalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalProjectApp.swift
+//  PersonalProject
+//
+//  Created by Pedro Vargas on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
